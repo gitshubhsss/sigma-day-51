e61ef6718b397434d81ac65d11627d028083edf6
gitshubhsss <shubhamranjane16@gmail.com>
2024-04-04 20:24:33 +0530

    search the collages on the basis of it state name

---
+++ QueryFolder/app.js
@@ -0,0 +1,31 @@
+let url = "http://universities.hipolabs.com/search?country=India&name=";
+let btn = document.querySelector("button");
+let inp=document.querySelector("input")
+let ol=document.querySelector("#list");
+
+
+btn.addEventListener("click", async () => {
+  let state = inp.value;
+  console.log("Statename : ",state);
+  let collages = await getCollages(state);
+  Show(collages)
+});
+
+function Show(collages) {
+    ol.innerText="";
+    for (col of collages) {
+        let li=document.createElement("li");
+        ol.appendChild(li);
+        li.innerText=col.name;
+    }
+}
+
+async function getCollages(state) {
+  try {
+    let res = await axios.get(url + state);
+    console.log(res);
+    return res.data;
+  } catch (error) {
+    console.log("error occured");
+  }
+}
