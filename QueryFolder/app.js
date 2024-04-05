500c74c2ec6c214cb164a76c417cad01a94e07ab
gitshubhsss <shubhamranjane16@gmail.com>
2024-04-05 18:39:43 +0530

    some changes

--- QueryFolder/app.js
+++ QueryFolder/app.js
@@ -3,23 +3,6 @@ let btn = document.querySelector("button");
 let inp=document.querySelector("input")
 let ol=document.querySelector("#list");
 
-
-btn.addEventListener("click", async () => {
-  let state = inp.value;
-  console.log("Statename : ",state);
-  let collages = await getCollages(state);
-  Show(collages)
-});
-
-function Show(collages) {
-    ol.innerText="";
-    for (col of collages) {
-        let li=document.createElement("li");
-        ol.appendChild(li);
-        li.innerText=col.name;
-    }
-}
-
 async function getCollages(state) {
   try {
     let res = await axios.get(url + state);
@@ -29,3 +12,21 @@ async function getCollages(state) {
     console.log("error occured");
   }
 }
+function Show(collages) {
+  ol.innerText="";
+  for (col of collages) {
+      let li=document.createElement("li");
+      ol.appendChild(li);
+      li.innerText=col.name;
+  }
+}
+
+btn.addEventListener("click", async () => {
+  let state = inp.value;
+  console.log("Statename : ",state);
+  let collages = await getCollages(state);
+  Show(collages)
+});
+
+
+
